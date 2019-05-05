d72bfc5ae8ffd540f25c2234c6602c0159ef51d0
Camilo <ca-milin15@hotmail.com>
2019-05-05 01:37:48 -0500

    Dia 5 aprendizaje
    LocalStorage, filtros, watch, watchCollection

--- toDoList/controller.js
+++ toDoList/controller.js
@@ -1,10 +1,19 @@
 var app = angular.module("ToDoList", ["LocalStorageModule"]);
 
 app.controller('secondController', function($scope, localStorageService){
+ 
  $scope.todo = [];
+ if(localStorageService.get("todo-list")){
+    $scope.todo = localStorageService.get("todo-list");
+ }
+
+ $scope.$watchCollection('todo-list', function(){
+     console.log($scope.todo);
+    localStorageService.set("todo-list", $scope.todo);
+ });
 
  $scope.addActv = function(){
-     $scope.todo.push($scope.newactv);
-     $scope.newactv= {};
+    $scope.todo.push($scope.newActv);
+    $scope.newactv= {};
  }
-})
+}) 
